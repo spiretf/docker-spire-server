e6a1a11ab64453cf86f3a86d43ff539cb7d3c6ed
Robin Appelman <icewind@owncloud.com>
2016-01-05 18:58:26 +0100

    fix config defaults

--- tf.sh
+++ tf.sh
@@ -4,12 +4,12 @@ CFG="$SERVER/tf2/tf/cfg/server.cfg"
 
 # Configure server from ENV
 
-echo "hostname \"${NAME:-'Spire Server'}\"" >> $CFG
-echo "sv_password \"${PASSWORD:-'password'}\"" >> $CFG
-echo "rcon_password \"${RCON_PASSWORD:-'rcon'}\"" >> $CFG
-echo "sm_webrcon_key \"${WEBRCON_PASSWORD:-${RCON_PASSWORD:-'rcon'}}\"" >> $CFG
+echo "hostname \"${NAME:-"Spire Server"}\"" >> $CFG
+echo "sv_password \"${PASSWORD:-password}\"" >> $CFG
+echo "rcon_password \"${RCON_PASSWORD:-rcon}\"" >> $CFG
+echo "sm_webrcon_key \"${WEBRCON_PASSWORD:-${RCON_PASSWORD:-rcon}}\"" >> $CFG
 echo "logstf_apikey \"${LOGSTF_APIKEY}\"" >> $CFG
 echo "exec ${CONFIG:-etf2l_6v6_5cp}" >> $CFG
 
 cd $HOME/hlserver
-tf2/srcds_run -game tf -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/tf2_ds.txt +map ${MAP:-'cp_badlands'} $@
+tf2/srcds_run -game tf -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/tf2_ds.txt +map ${MAP:-cp_badlands} $@
